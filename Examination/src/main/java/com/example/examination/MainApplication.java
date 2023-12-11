b2d7ae8efafb5f5a865a9e648968bf6cec662b92
Vero <vskurtmullaeva@stud.kantiana.ru>
2023-12-12 00:35:06 +0200

    Разработана стартовая страница и добавлена следующая

--- Examination/src/main/java/com/example/examination/MainApplication.java
+++ Examination/src/main/java/com/example/examination/MainApplication.java
@@ -3,21 +3,33 @@ package com.example.examination;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class MainApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Pane root = loader.<Pane>load();
+
+            Scene scene = new Scene(root, 457, 650);
+
+            primaryStage.setResizable(false);
+
+            primaryStage.setScene(scene);
+
+            primaryStage.setTitle("FilmFinder");
+
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
